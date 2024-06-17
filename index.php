83985e49811f5782ffdd8ad8e2ff025f385a195a
ELAINERAFAEL <162643406+ELAINERAFAEL@users.noreply.github.com>
2024-06-17 14:25:51 -0300

    Correção
    Correção na posição do footer

--- index.php
+++ index.php
@@ -68,10 +68,6 @@ if (file_exists($pagina)){
 ?>
 </main>
 
-<footer class="footer">
-<p>desenvolvido por</p>
-</footer>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -189,6 +185,11 @@ if (file_exists($pagina)){
       </p>
     </div>
   </div>
+
+  
+  <footer class="footer">
+  <p>desenvolvido por</p>
+  </footer>
 </main>
 </body>
 </html>
