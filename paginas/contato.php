4c5d9abd4d454adf421d16be5f37b2263355f87b
ELAINERAFAEL <162643406+ELAINERAFAEL@users.noreply.github.com>
2024-06-21 14:21:51 -0300

    atualização
    ajustes na pagina de contato

--- paginas/contato.php
+++ paginas/contato.php
@@ -1,1 +1,22 @@
-<h2>Entre em contato</h2>
+<h1>Fale Conosco</h1>
+    <br>
+    <form name="formContato" type="submit" action="obrigado">
+        <div class="for">
+            <label for="nome">Digite seu nome completo:</label>
+            <input type="text" name="nome" id="nome" required>
+        <br></div>
+        <div class="for">
+            <label for="email">Digite seu email:</label>
+            <input type="email" name="email" id="email" required>
+        <br></div>
+        <div class="for">
+            <label for="mensagem">degite sua mensagem:</label>
+            <textarea name="mensagem" id="mensagem" required rows="10"></textarea>
+        <br></div>
+        <div class="for">
+            <button type="submit">
+                Enviar mensagem
+            </button>
+        </div>
+    </form>
+    <br>
---
+++ paginas/obrigado.php
@@ -0,0 +1,7 @@
+
+    <br>
+    <div>
+        <img src="./imagens/obrigado.jpeg" class="banner" style="height: 500px;"></img> 
+    </div>
+    <br>
+  
